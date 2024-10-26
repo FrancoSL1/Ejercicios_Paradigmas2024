645904044a84f0398ce59b5055dfe264c63148c4
Franco Simon <hdp9951@gmail.com>
2024-10-25 21:24:18 -0300

    Aquí agregue el ejercicio

---
+++ Ejercicio1.py
@@ -0,0 +1,15 @@
+#Ingreso de datos
+temperatura = float(input("Ingrese la temperatura en grados Celsius: "))
+
+#Categorias de temperatura
+if temperatura <= 0:
+    print("La temperatura de: ", temperatura,"°C es congelante.")
+
+elif temperatura > 0 and temperatura <= 15:
+    print("La temperatura de: ", temperatura,"°C es fria.")
+
+elif temperatura >= 16 and temperatura <= 25:
+    print("La temperatura de: ", temperatura,"°C es templada")
+
+else:
+    print("La temperatura de: ", temperatura,"°C es calida")
